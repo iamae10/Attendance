6ccf192639ac5946cf48e653e85c396c591b0222
ia_mae <84659066+iamae10@users.noreply.github.com>
2023-05-29 15:12:45 +0800

    Update Plane.java

--- code Inheritance/Plane.java
+++ code Inheritance/Plane.java
@@ -1,5 +1,20 @@
 package IndividualActivity;
 
-public class Plane {
+public class Plane extends Vehicle {
+   
+  double wingspan; 
+	 
+	public Plane(int speed, String color, int price, double wingspan) {
+		super(speed, color, price);
+		this.wingspan = wingspan;
+	}
+	   //add method
+		public void fly() {
+			   System.out.print("The plane is flying"); 
+		     }
 
+		@Override  // method inherited from vehicle class
+		public void stop() {
+			System.out.print("The plane stop"); 
+		}
 }
