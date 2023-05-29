a75229668f55897ab9bd7b47f604c06ff4fa5143
ia_mae <84659066+iamae10@users.noreply.github.com>
2023-05-29 15:15:51 +0800

    Update U2SpyPlane.java

--- code Inheritance/U2SpyPlane.java
+++ code Inheritance/U2SpyPlane.java
@@ -1,6 +1,6 @@
 package IndividualActivity;
 
-public class U2SpyPlane extends Vehicle
+public class U2SpyPlane extends Plane
 {
      public U2SpyPlane(int speed, String color, int price, double wingspan) {
 		super(speed, color, price, wingspan);
