a62c0757ac5b5d691a9cd306bf0b02ef5d7f68bf
ia_mae <84659066+iamae10@users.noreply.github.com>
2023-05-29 15:09:53 +0800

    Update main.java

--- code Inheritance/main.java
+++ code Inheritance/main.java
@@ -3,14 +3,14 @@ package IndividualActivity;
 public class main {
 
 	public static void main(String[] args) {
-		//instance from the subclass
+		
 		U2SpyPlane plane = new U2SpyPlane(1000, "white", 100000, 50.5);
 		//print out the properties for plane
 		System.out.println("The " + plane.color + " U-2 Spy Plane is worth of" + plane.price + 
 				" pesos, have the wingspan of \n" + plane.wingsSpan + 
 				" ft. with the speed of " + plane.speed);
 		
-		//display the method
+		//display the method that inherited from a U2SpyPlane class
 		plane.fly();
     plane.stop();
        
