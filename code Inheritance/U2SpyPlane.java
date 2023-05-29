b2318b68cbf083c4810c67a169a1d51348b38393
ia_mae <84659066+iamae10@users.noreply.github.com>
2023-05-29 15:15:19 +0800

    Update U2SpyPlane.java

--- code Inheritance/U2SpyPlane.java
+++ code Inheritance/U2SpyPlane.java
@@ -2,21 +2,8 @@ package IndividualActivity;
 
 public class U2SpyPlane extends Vehicle
 {
-    double wingsSpan; 
-     
-	public U2SpyPlane(int speed, String color, int price, double wingsSpan) {
-		super(speed, color, price);
-		this.wingsSpan = wingsSpan;
-	 }
-     
-	public void fly() {
-	   System.out.print("The plane is flying"); 
-
-     }
-     @Override
-     public void stop() {
-	 System.out.print("The plane stopped"); 
-     
-       }
+     public U2SpyPlane(int speed, String color, int price, double wingspan) {
+		super(speed, color, price, wingspan);
+	}
 }
 
